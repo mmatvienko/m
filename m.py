7b458f3e3321a1b2c912d9776cc6d16ce8377f7b
Marc Matvienko <m.matvienko@utexas.edu>
2018-10-19 16:40:45 -0500

    supporting clean up

--- m.py
+++ m.py
@@ -1,19 +1,14 @@
 # This file should hopefully run everything... probably good if I design everything first
 from accounts import AlphaVantage
-from utils import Security, Portfolio
-import time
+from utils import Security, Portfolio, Strategy
 
 apikey = AlphaVantage.apiKey
 
 
 # have a thing that handles a queue of order and this object can only run when markets are open.
 # so queue fills when markets closed
-goog = Security("GOOG")
 portfolio = Portfolio(1000)
-while True:
-    print(goog.get_price())
-    if goog.get_price() < 6:
-        portfolio.buy(goog, 3)
-    if goog.get_price() > 27:
-        portfolio.sell_all(goog)
-    time.sleep(1)
+strategy = Strategy()
+
+# spawn a thread to run strategy
+strategy.execute(portfolio)
