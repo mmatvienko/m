0b6ac09e24a4cd83ccf5aa8c149a36ced3543ef9
Marc Matvienko <m.matvienko@utexas.edu>
2018-10-20 18:39:30 -0500

    basic boundry strat

--- strategy.py
+++ strategy.py
@@ -4,33 +4,41 @@ from security import Security
 
 class Strategy():
     def __init__(self):
+        # TODO framework to test different parameter in parallel
         self.HZ = 5.0
         self.price_sum = 0
         # perhaps make arg in Q(arg) an arg in __init__
-        self.prices = AutoQueue(20)
+        self.prices = AutoQueue(10)
         self.price_sum_sq = 0
         self.count = 0
+        self.boundary = 0.05    # percent change from mean
 
     # calculate average using a queue, be able to set size using some parameter
     def execute(self, portfolio):
         goog = Security("GOOG")
-        while True:
+        counter = 0
+        while counter < 750:
             price = goog.get_price()
-
-            # make space in queue if necessary
             self.prices.put(price)
+
             # do things necessary for avg and stddev calc
-            # perhaps make these calculations more efficient?
-            # is O(1) tho as long as AutoQueue.size doesn't scale
             self.price_sum = self.prices.sum()
             self.price_sum_sq = self.prices.sum_sq()
             self.count = len(self.prices)
 
-            # if goog.get_price() < 6:
-            #     portfolio.buy(goog, 3)
-            # if goog.get_price() > 27:
-            #     portfolio.sell_all(goog)
-            avg = self.price_sum / self.count
-            var = (self.price_sum_sq - ((self.price_sum)**2)/self.count) / self.count
+            avg = self.prices.average()
+            var = self.prices.variance()
+            
+            if price < avg * (1 - self.boundary):
+                # low price, want to buy
+                while portfolio.buy(goog, 1): pass        
+                self.holding = True
+            elif price > avg * (1 + self.boundary):
+                # high price, want to sell_all
+                portfolio.sell_all(goog)
+                self.holding = False
             print(f'Price: {price:.2f}\tAvg: {avg:.2f}\tvar: {var:.2f}')
-            sleep(1.0/self.HZ)
+            print(f'while counter: {counter}')
+            counter += 1
+            # sleep(1.0/self.HZ)
+        print(portfolio)
