ffd7351f9fde574ca4d50b872fbc457d821785b7
Marc Matvienko <m.matvienko@utexas.edu>
2018-10-20 18:38:10 -0500

    call security.price instead of get_price

--- portfolio.py
+++ portfolio.py
@@ -1,15 +1,15 @@
-DEBUG = False
+DEBUG = True
 class Portfolio:
     def __init__(self, balance):
         self.balance = balance
         self.holdings = {}
-
+        self.trades_made = 0
 
     def buy(self, security, quantity):
         if not self._can_purchase(security, quantity):
             return False
         # check if you can actully purchase
-        purchase_price = security.get_price()
+        purchase_price = security.price
         self.balance -= purchase_price * quantity
         if security in self.holdings:
             self.holdings[security] += quantity
@@ -17,17 +17,19 @@ class Portfolio:
             self.holdings[security] = quantity
         if DEBUG:
             print(f'purchased {quantity} shares of {security.ticker} at {purchase_price}')
-            print(f'balance is now {self.balance}')
+            print(self.__str__())
         # maybe track the purchase in a serializable python class
+        self.trades_made += 1
         return True
 
 
     def sell_all(self, security):
         if security in self.holdings:
-            sale_price = security.get_price()
+            sale_price = security.price
             quantity = self.holdings[security]
             self.balance += sale_price * quantity
             del self.holdings[security]
+            self.trades_made += 1
             if DEBUG:
                 print(f'sold {quantity}x{security.ticker} for {sale_price*quantity}')
                 print(f'balance is now {self.balance}')
@@ -37,4 +39,8 @@ class Portfolio:
 
     def _can_purchase(self, security, quantity):
         # define this based on whatever metric
-        return self.balance > security.get_price() * quantity
+        return self.balance > security.price * quantity
+
+    def __str__(self):
+        total_equity = sum([security.price * self.holdings[security] for security in self.holdings])
+        return f'Cash: {self.balance:.2f}\tEquity: {total_equity}\tTotal: {(total_equity + self.balance):.2f}\tTrades made: {self.trades_made}'
