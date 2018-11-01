f32f12baf72540e4da3288cc18e11efc39e0e691
Marc Matvienko <m.matvienko@utexas.edu>
2018-11-01 18:16:10 -0500

    works with IEXFinance

--- security.py
+++ security.py
@@ -1,4 +1,39 @@
-class Security:
+from iexfinance import Stock
+
+class IEXSecurity:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.security = Stock(ticker)
+        
+    def get_price(self):
+        return self.security.get_price()
+
+
+class IEXTestSecurity:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.price = None        
+        self.prices = None
+        from iexfinance import get_historical_data
+        from datetime import datetime
+        start = datetime(2014, 1, 1)
+        end = datetime(2018, 1, 1)
+        df = get_historical_data(self.ticker, start=start, end=end, output_format='pandas')
+        self.prices = df['open']
+        print("Opening: ", self.prices[0])
+        print("Closing: ", self.prices[-1])
+        print("Days: ", len(self.prices))
+        self.counter = 0
+
+    def get_price(self):
+        # do price grabbing stuff like talking to an API
+        # self.price = time() % 32
+        self.price = self.prices[self.counter]
+        self.counter += 1
+        return self.price # maybe append timestamp
+
+
+class TestSecurity:
     def __init__(self, ticker):
         self.ticker = ticker
         self.price = None
--- strategy.py
+++ strategy.py
@@ -1,15 +1,63 @@
 from time import sleep
 from utils import AutoQueue
-from security import Security
+from security import IEXTestSecurity as TestSecurity
+from security import IEXSecurity as Security
 
-class BlankStrat():
+import time
+
+class LiveTestStrat():
+    """Same as Previous strat but this should use live prices
+    and trade the same stratey as Strategy()
+    """
     def __init__(self):
-        pass
+        self.HZ = 1.0/1.0
+        self.local_strategies = [self.tsla]
+
+        # variables needed for tsla()
+        self.prices = AutoQueue(10)
+        self.boundary = 0.01
+        self.holding = False
+        self.volume = 0
+    def execute(self, portfolio):
+        while True:
+            for local_strategy in self.local_strategies:
+                local_strategy()
+            sleep(1.0/self.HZ)
+
+    def tsla(self):
+        tsla = Security('TSLA') 
+        price = tsla.get_price()
+        self.prices.put(price)
+        volume = tsla.security.get_volume()
+        # if volume != self.volume:
+        #     print(volume)
+        #     self.volume = volume
+        # else:
+        #     return
+
+        # do things necessary for avg and stddev calc
+        # self.price_sum = self.prices.sum()
+        # self.price_sum_sq = self.prices.sum_sq()
+        # self.count = len(self.prices)
+        
+        avg = self.prices.average()
+        var = self.prices.variance()
+    
+        if price < avg * (1 - self.boundary):
+            # low price, want to buy
+            while portfolio.buy(tsla, 1): pass        
+            self.holding = True
+        elif price > avg * (1 + self.boundary):
+            # high price, want to sell_all
+            portfolio.sell_all(tsla)
+            self.holding = False
+        print(f'Price: {price:.2f}\tAvg: {avg:.2f}\tvar: {var:.2f} <{time.asctime()}', end='\r')
+       
 
 class Strategy():
     def __init__(self):
         # TODO framework to test different parameter in parallel
-        self.HZ = 140.0
+        self.HZ = 14000.0
         self.price_sum = 0
         # perhaps make arg in Q(arg) an arg in __init__
         self.prices = AutoQueue(10)
@@ -19,9 +67,9 @@ class Strategy():
 
     # calculate average using a queue, be able to set size using some parameter
     def execute(self, portfolio):
-        goog = Security("GOOG")
+        goog = TestSecurity("GOOG")
         counter = 0
-        while counter < 750:
+        while counter < 949:
             price = goog.get_price()
             self.prices.put(price)
 
